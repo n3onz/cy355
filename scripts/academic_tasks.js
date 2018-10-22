99b4b8dbbaeb5e5acfb2526a6deb92cd4322e224
n3onz <neonpuzzler@gmail.com>
2018-10-22 12:52:03 -0400

    New files

---
+++ scripts/academic_tasks.js
@@ -0,0 +1,6 @@
+var academicTaskList = {"academicTaskItems":[
+	{"AcademicTask":"CY355 Project", "Category":""},
+	{"AcademicTask":"LA203 WPR", "Category":"important"},
+	{"AcademicTask":"CH102 Homework", "Category":""},
+	{"AcademicTask":"Meet with DAC", "Category":""}
+]};
